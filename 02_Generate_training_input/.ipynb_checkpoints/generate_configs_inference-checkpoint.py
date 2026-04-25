e82efc4b9203af6c55c9843731fd80683a7addc2
xwanaf <maxwan@ust.hk>
2026-04-26 07:45:36 +0800

    Fix bugs

--- 02_Generate_training_input/.ipynb_checkpoints/generate_configs_inference-checkpoint.py
+++ 02_Generate_training_input/.ipynb_checkpoints/generate_configs_inference-checkpoint.py
@@ -24,7 +24,9 @@ from scipy.sparse import issparse
 
 
 import sys
-sys.path.append('/home/xwanaf/bio/scGPT-dev-temp/Atlas_integration/Reproducibility/Validation')
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 from UniGeneX.utils import *
 
 import argparse
